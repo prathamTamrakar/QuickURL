9703d420dec4a073328d6a213976dcabfaec330b
Pratham Tamrakar <prathamtamrakar849@gmail.com>
2026-01-12 17:55:55 +0530

    configure files for short-url

---
+++ Backend/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const cookieParser = require("cookie-parser")
+const {restricttoLoggedInUserOnly, checkAuth} = require("./middleware/auth")
+const urlRoute = require("./routes/url")
+const staticRoute = require("./routes/staticRouter")
+const userRoute = require("./routes/user")
+
+const {connectToMongoDB} = require("./connect")
+const URL = require("./models/url")
+
+connectToMongoDB(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
+.then(()=> console.log("MongoDB Connected"))
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
+
+// app.get('/test',async (req,res)=>{
+//     const allUrls = await URL.find({})
+//     return res.render("home",{
+//         urls: allUrls,
+//     })
+// })
+
+
+app.use("/url",restricttoLoggedInUserOnly,urlRoute)
+app.use("/",staticRoute)
+app.use("/user",userRoute)
+
+app.get("/url/:shortId", async (req,res)=>{
+    const shortId = req.params.shortId
+    const entry = await URL.findOneAndUpdate({
+        shortId,
+    },{ 
+        $push: {
+            visitedHistory:{
+                timestamp: Date.now()
+            }
+        }
+    })
+    res.redirect(entry.redirectedUrl)
+})
+
+app.listen(process.env.PORT, ()=>console.log(`Server started on the PORT: ${process.env.PORT}`))
